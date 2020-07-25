9b70abc8f4a9c4d8bac472b9b492356a9fa4ed10
Matthew Jacques <iFtw@live.co.uk>
2020-07-25 10:04:55 +0100

    Implement GameListEntry object (#1)

---
+++ DesktopApplication/Models/GameListEntry.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopApplication.Models
+{
+  public class GameListEntry
+  {
+    public enum Status { NOTPLAYED, PLAYED, COMPLETE, ABANDONED }
+
+    #region Properties
+
+    private String _name = String.Empty;
+    /// <summary>
+    /// Get/set the title of the game
+    /// </summary>
+    public String Name { get { return _name; } set { _name = value; } }
+
+    private bool _isOnPS4 = false;
+    /// <summary>
+    /// Get/set if the game is playable on PS4
+    /// </summary>
+    public bool IsOnPS4 { get { return _isOnPS4; } set { _isOnPS4 = value; } }
+
+    private bool _isOnPS3 = false;
+    /// <summary>
+    /// Get/set if the game is playable on PS3
+    /// </summary>
+    public bool IsOnPS3 { get { return _isOnPS3; } set { _isOnPS3 = value; } }
+
+    private bool _isOnPSVita = false;
+    /// <summary>
+    /// Get/set if the game is playable on PS Vita
+    /// </summary>
+    public bool IsOnPSVita { get { return _isOnPSVita; } set { _isOnPSVita = value; } }
+
+    private bool _isOnPC = false;
+    /// <summary>
+    /// Get/set if the game is playable on PC
+    /// </summary>
+    public bool IsOnPC { get { return _isOnPC; } set { _isOnPC = value; } }
+
+    private Status _playStatus = Status.NOTPLAYED;
+    /// <summary>
+    /// Get/set the play status of the game
+    /// </summary>
+    public Status PlayStatus { get { return _playStatus; } set { _playStatus = value; } }
+
+    private bool _owned = false;
+    /// <summary>
+    /// Get/set the if the game is owned
+    /// </summary>
+    public bool Owned { get { return _owned; } set { _owned = value; } }
+
+    private DateTime _dateAdded = DateTime.Today;
+    /// <summary>
+    /// Get/set the date the game was added to the database
+    /// </summary>
+    public DateTime DateAdded { get { return _dateAdded; } set { _dateAdded = value; } }
+
+    #endregion
+  }
+}
