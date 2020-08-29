060d334f4f62c802167c20095fbff131298feaa8
Matthew Jacques <iFtw@live.co.uk>
2020-08-29 12:02:32 +0100

    Implement ability to switch views within a window (#8)
    * Added Hamburger Menu
    
    * Added ApplicationView and ApplicationViewModel
    
    * GameListView is now shown through the ApplicationWindow
    
    * Sorted out formatting
    
    * Fixed StretchControl dependency property
    
    * Make sure control name is set in StretchControl
    
    * Added buttons to hamburger menu that can switch between empty StatsView and GameListView

---
+++ Desktop/ViewModels/StatsViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Desktop.Data.Types;
+using Desktop.Extensions.Helpers;
+using Desktop.Interfaces;
+using Desktop.Models;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Desktop.ViewModels
+{
+  public class StatsViewModel : ViewModelBase, IPageViewModel
+  {
+    #region Variables
+
+    
+
+    #endregion // Variables
+
+    #region Construction
+
+    
+
+    #endregion // Construction
+
+    #region Properties
+
+    private string _name = "Stats";
+    /// <summary>
+    /// The name of the view model
+    /// </summary>
+    public string Name { get { return _name; } set { _name = value; } }
+
+    #endregion // Properties
+
+    #region Commands
+
+
+
+    #endregion // Commands
+
+    #region Implementation
+
+    
+
+    #endregion // Implementation
+  }
+}
