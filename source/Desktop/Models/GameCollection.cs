d16c8ea695b6ba98a2ba2c02eecf25f8a702730f
Matthew Jacques <iFtw@live.co.uk>
2020-10-20 19:33:33 +0100

    Update views with regions, add GameCollection.cs

---
+++ source/Desktop/Models/GameCollection.cs
@@ -0,0 +1,33 @@
+﻿using Desktop.Extensions.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop.Models
+{
+  public class GameCollection
+  {
+    #region Private Members
+
+    private List<string> _games = new List<string>();
+    private string _name = "";
+
+    #endregion Private Members
+
+    #region Public Properties
+
+    /// <summary>
+    /// List of game names in the collection
+    /// </summary>
+    public List<string> Games { get => _games; set => _games = value; }
+
+    /// <summary>
+    /// Name of the collection
+    /// </summary>
+    public string Name { get => _name; set => _name = value; }
+
+    #endregion Public Properties
+  }
+}
